464486773a3aa5fb79839853f91f1a8b1ad29253
diazddavid <diazrivasdavid5@gmail.com>
2022-12-05 17:10:32 +0100

    yaml mod 7

--- cerebroDigital/settings.py
+++ cerebroDigital/settings.py
@@ -128,13 +128,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
 STATIC_URL = '/static/'
 # Following settings only make sense on production and may break development environments.
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
